49ee247ae81ac8b3f63f2527b87ec8de4aaadf37
Humayun Ahmed <humayun.ahmed@banglalink.net>
2023-07-11 13:41:40 +0600

    added- getUsers,getUserByID,PostUsers,UserNotFoundException

---
+++ src/main/java/com/humayun/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,33 @@
+package com.humayun.rest.webservices.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
+
+    static {
+        users.add(new User(++usersCount, "Humayun", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Fatema", LocalDate.now().minusYears(18)));
+        users.add(new User(++usersCount, "Shishir", LocalDate.now().minusYears(30)));
+    }
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+}
---
+++ src/main/java/com/humayun/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,39 @@
+package com.humayun.rest.webservices.restfulwebservices.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+        private UserDaoService service;
+        public UserResource(UserDaoService service) {
+            this.service = service;
+        }
+    // GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET /users/{id}
+    @GetMapping("/users/{id}")
+    public User retrieveUsers(@PathVariable int id) {
+        User user = service.findOne(id);
+        if(user==null)
+            throw new UserNotFoundException("id:" + id);
+        return user;    }
+
+    //POST /users
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUsers = service.save(user);
+
+        // Returns status & Location - /users/{id}
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUsers.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
